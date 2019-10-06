548396e47d8f404843f9cb06a243464ecd7dbe16
ioana01 <55446346+ioana01@users.noreply.github.com>
2019-10-06 14:36:23 +0300

    Add files via upload

---
+++ Evrika.js
@@ -0,0 +1,39 @@
+function Rezultat()
+{
+    window.location = 'results.html';
+}
+
+function Inscriere()
+{
+    window.location = 'inscriere.html';
+}
+
+function Despre()
+{
+    window.location = 'home.html';
+}
+
+function FAQ()
+{
+    window.location = 'FAQ.html';
+}
+
+function Contact()
+{
+    window.location = 'contact.html';
+}
+
+function Login()
+{
+    window.location = 'login.html';
+}
+
+function Start()
+{
+    window.location = 'intrebari.html';
+}
+
+function inapoi()
+{
+    window.location = 'home.html';
+}
